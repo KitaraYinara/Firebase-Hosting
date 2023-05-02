18318ce636dd8f0aa1cf110ffd1af14007d4635c
Bensonqjy <117067688+Bensonqjy@users.noreply.github.com>
2023-05-02 22:41:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/patient" element={<Patient />} />
+      <Route path="/staff" element={<Staff />} />
     </Routes>
   );
 }
