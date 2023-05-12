9e99951d05becfaf23327a4e205030ae7ce4da9e
Abhi4201790 <116883713+Abhi4201790@users.noreply.github.com>
2023-05-12 10:12:31 +0800

    Add files via upload

---
+++ src/components/Navigation/Authentication.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { auth, app } from "../../firebase"
+import Nav from "react-bootstrap/Nav";
+import { onAuthStateChanged, signOut  } from "firebase/auth";
+
+const Authentication = () => {
+    const [authenticatedUser, setauthenticatedUser] = useState("");
+
+    useEffect(() => {
+      const listenAuth = onAuthStateChanged(auth, (user) => {
+        if (user){
+          setauthenticatedUser(user)
+        }else {
+          setauthenticatedUser(null)
+        }
+      }
+      )
+      return () => {
+        listenAuth();
+      }
+    },[])
+
+    const userSignOut = () => {
+      signOut(auth).then(() => {
+        //console.log("user signed out")
+      }).catch(error => console.log("error"))
+    }
+
+
+  return (
+    <>
+    { authenticatedUser === null ?
+    <>
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/signup">Sign up</Nav.Link>
+    </> :
+      <>
+      <Nav.Link href="/" onClick={userSignOut}>Sign Out</Nav.Link>
+  </> 
+      }
+
+    </>
+
+  );
+};
+
+export default Authentication;
