28bb2fb6849abd6b27dc033d07e29f7c4c96ed75
Abhi4201790 <116883713+Abhi4201790@users.noreply.github.com>
2023-05-12 10:16:40 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,6 +12,7 @@ const firebaseConfig = {
   storageBucket: "fir-authentication-96cd6.appspot.com",
   messagingSenderId: "140073163102",
   appId: "1:140073163102:web:0a9b1ad84ab1d0308497cf",
+  databaseURL: "https://fir-authentication-96cd6-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 // Initialize Firebase
@@ -21,3 +21,4 @@ const auth = getAuth(app);
 
 export { app, auth };
 export const db = getFirestore(app);
+export const db1 = firebase.initializeApp(firebaseConfig, "second_db").database();
