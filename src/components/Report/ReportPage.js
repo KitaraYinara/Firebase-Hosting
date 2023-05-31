0d9d3b1ca20aa262bf9ad09c6e082622f19c2288
hein <2103553J@student.tp.edu.sg>
2023-05-31 13:39:48 +0800

    styled button

--- src/components/Report/ReportPage.js
+++ src/components/Report/ReportPage.js
@@ -302,13 +302,10 @@ function ReportPage() {
               width="30"
               height="30"
               fill="currentColor"
-              class="bi bi-arrow-return-left"
-              viewBox="0 0 22 18"
+              class="bi bi-view-list"
+              viewBox="0 0 20 18"
             >
-              <path
-                fill-rule="evenodd"
-                d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
-              />
+              <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2zm0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14z" />
             </svg>
             Main Report
           </button>
