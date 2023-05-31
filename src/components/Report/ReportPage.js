834c65a2f7e8c9bf5e18264d34b064bc18c4909f
hein <2103553J@student.tp.edu.sg>
2023-05-31 13:37:32 +0800

    Styled report page

--- src/components/Report/ReportPage.js
+++ src/components/Report/ReportPage.js
@@ -11,11 +11,16 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+import ExcelJS from "exceljs";
+import "./ReportPage.css";
 
 function ReportPage() {
   const { patientId, testId } = useParams();
   const [sensorData, setSensorData] = useState([]);
   const [patientName, setPatientName] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const pageSize = 10;
+  const [showAllData, setShowAllData] = useState(false);
 
   useEffect(() => {
     const fetchSensorData = () => {
@@ -30,13 +35,16 @@ function ReportPage() {
       const sensorsQuery = query(sensorsCollectionRef);
 
       const unsubscribe = onSnapshot(sensorsQuery, (snapshot) => {
-        const sensorDataList = snapshot.docs.map((doc) => ({
-          timestamp: doc.data().timestamp.toDate().toLocaleString(),
-          bpm: doc.data().bpm,
-          spO2: doc.data().spO2,
-          motion: doc.data().motion,
-        }));
+        const sensorDataList = snapshot.docs
+          .map((doc) => ({
+            timestamp: doc.data().timestamp.toDate(),
+            bpm: doc.data().bpm,
+            spO2: doc.data().spO2,
+            motion: doc.data().motion,
+          }))
+          .sort((a, b) => a.timestamp - b.timestamp); // Sort the sensor data by timestamp
         setSensorData(sensorDataList);
+        setCurrentPage(0);
       });
 
       return () => {
@@ -57,6 +65,67 @@ function ReportPage() {
     fetchSensorData();
     fetchPatientName();
   }, [patientId, testId]);
+
+  const handleDownloadExcel = () => {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet("Sensor Data");
+    const headers = ["Timestamp", "Pulse Rate", "Oxygen Level", "Motion"];
+    worksheet.addRow(headers);
+
+    sensorData.forEach((data) => {
+      const row = [
+        data.timestamp.toLocaleString(),
+        data.bpm,
+        data.spO2,
+        data.motion,
+      ];
+      worksheet.addRow(row);
+    });
+
+    const fileName = `SensorData_Test${testId}.xlsx`;
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const data = new Blob([buffer], { type: "application/octet-stream" });
+
+      if (navigator.msSaveBlob) {
+        // For IE 10+
+        navigator.msSaveBlob(data, fileName);
+      } else {
+        // For other browsers
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(data);
+        link.download = fileName;
+        link.click();
+        window.URL.revokeObjectURL(link.href);
+      }
+    });
+  };
+
+  const handleShowAllData = () => {
+    setShowAllData(true);
+  };
+
+  const handleGoBack = () => {
+    setShowAllData(false);
+  };
+
+  const handlePageChange = (direction) => {
+    const maxPage = Math.floor((sensorData.length - 1) / pageSize);
+    if (direction === "prev" && currentPage > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    } else if (direction === "next" && currentPage < maxPage) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  };
+
+  const startIdx = currentPage * pageSize;
+  const endIdx = startIdx + pageSize;
+  const currentPageData = sensorData.slice(startIdx, endIdx);
+  const maxPage = Math.floor((sensorData.length - 1) / pageSize);
   const routeToTests = () => {
     window.location.href = `/patients/${patientId}/tests`;
   };
@@ -64,67 +133,188 @@ function ReportPage() {
     window.location.href = `/mainreport/${patientId}/${testId}`;
   };
   return (
-    <>
-      <div>
-        <h1 className="pageheader">
-          Report for Patient: {patientName}, Test ID: {testId}
-        </h1>
-        {sensorData.length === 0 ? (
-          <p>Loading sensor data...</p>
-        ) : (
-          <LineChart width={1900} height={800} data={sensorData}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="timestamp" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="bpm" stroke="#8884d8" />
-            <Line type="monotone" dataKey="spO2" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="motion" stroke="#ffc658" />
-          </LineChart>
-        )}
-      </div>{" "}
-      <button
-        variant="primary"
-        className="back"
-        onClick={(e) => routeToTests()}
-      >
+    <div>
+      <h1 className="pageheader">
+        Report for Patient: {patientName}, Test ID: {testId}
+      </h1>
+      <button className="report-btn" onClick={handleDownloadExcel}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="30"
           height="30"
           fill="currentColor"
-          class="bi bi-arrow-return-left"
-          viewBox="0 0 22 18"
+          class="bi bi-filetype-csv"
+          viewBox="0 0 20 18"
         >
           <path
             fill-rule="evenodd"
-            d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+            d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM3.517 14.841a1.13 1.13 0 0 0 .401.823c.13.108.289.192.478.252.19.061.411.091.665.091.338 0 .624-.053.859-.158.236-.105.416-.252.539-.44.125-.189.187-.408.187-.656 0-.224-.045-.41-.134-.56a1.001 1.001 0 0 0-.375-.357 2.027 2.027 0 0 0-.566-.21l-.621-.144a.97.97 0 0 1-.404-.176.37.37 0 0 1-.144-.299c0-.156.062-.284.185-.384.125-.101.296-.152.512-.152.143 0 .266.023.37.068a.624.624 0 0 1 .246.181.56.56 0 0 1 .12.258h.75a1.092 1.092 0 0 0-.2-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.551.05-.776.15-.225.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.122.524.082.149.2.27.352.367.152.095.332.167.539.213l.618.144c.207.049.361.113.463.193a.387.387 0 0 1 .152.326.505.505 0 0 1-.085.29.559.559 0 0 1-.255.193c-.111.047-.249.07-.413.07-.117 0-.223-.013-.32-.04a.838.838 0 0 1-.248-.115.578.578 0 0 1-.255-.384h-.765ZM.806 13.693c0-.248.034-.46.102-.633a.868.868 0 0 1 .302-.399.814.814 0 0 1 .475-.137c.15 0 .283.032.398.097a.7.7 0 0 1 .272.26.85.85 0 0 1 .12.381h.765v-.072a1.33 1.33 0 0 0-.466-.964 1.441 1.441 0 0 0-.489-.272 1.838 1.838 0 0 0-.606-.097c-.356 0-.66.074-.911.223-.25.148-.44.359-.572.632-.13.274-.196.6-.196.979v.498c0 .379.064.704.193.976.131.271.322.48.572.626.25.145.554.217.914.217.293 0 .554-.055.785-.164.23-.11.414-.26.55-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.764a.799.799 0 0 1-.118.363.7.7 0 0 1-.272.25.874.874 0 0 1-.401.087.845.845 0 0 1-.478-.132.833.833 0 0 1-.299-.392 1.699 1.699 0 0 1-.102-.627v-.495Zm8.239 2.238h-.953l-1.338-3.999h.917l.896 3.138h.038l.888-3.138h.879l-1.327 4Z"
           />
         </svg>
-        Back to Tests
+        Download Excel
       </button>
-      <button
-        variant="primary"
-        className="view"
-        onClick={(e) => routeToMainReport()}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          fill="currentColor"
-          class="bi bi-arrow-right"
-          viewBox="0 0 22 18"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-          />
-        </svg>
-        Main Report
-      </button>
-    </>
+      {!showAllData && (
+        <button className="report-btn" onClick={handleShowAllData}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-graph-up"
+            viewBox="0 0 20 18"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z"
+            />
+          </svg>
+          Show All Data
+        </button>
+      )}
+      {showAllData && (
+        <button className="report-btn" onClick={handleGoBack}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-arrow-return-left"
+            viewBox="0 0 22 18"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+            />
+          </svg>
+          Back
+        </button>
+      )}
+      {sensorData.length === 0 ? (
+        <p>Loading sensor data...</p>
+      ) : (
+        <div>
+          {showAllData ? (
+            <div className="line-chart-container">
+              <LineChart width={1900} height={800} data={sensorData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="timestamp" tickFormatter={formatTimestamp} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="bpm"
+                  stroke="#8884d8"
+                  strokeWidth={2}
+                  dot={false}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="spO2"
+                  stroke="#82ca9d"
+                  strokeWidth={2}
+                  dot={false}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="motion"
+                  stroke="#ffc658"
+                  strokeWidth={2}
+                  dot={false}
+                />
+              </LineChart>
+            </div>
+          ) : (
+            <div className="line-chart-container">
+              <LineChart width={1900} height={800} data={currentPageData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="timestamp" tickFormatter={formatTimestamp} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="bpm"
+                  stroke="#8884d8"
+                  strokeWidth={2}
+                  dot={false}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="spO2"
+                  stroke="#82ca9d"
+                  strokeWidth={2}
+                  dot={false}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="motion"
+                  stroke="#ffc658"
+                  strokeWidth={2}
+                  dot={false}
+                />
+              </LineChart>
+            </div>
+          )}
+          <div>
+            <button
+              className="graph-btn"
+              onClick={() => handlePageChange("prev")}
+              disabled={currentPage === 0}
+            >
+              Prev
+            </button>
+            <button
+              className="graph-btn"
+              onClick={() => handlePageChange("next")}
+              disabled={currentPage === maxPage}
+            >
+              Next
+            </button>
+          </div>
+          <button
+            variant="primary"
+            className="back"
+            onClick={(e) => routeToTests()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-return-left"
+              viewBox="0 0 22 18"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+              />
+            </svg>
+            Back to Tests
+          </button>
+          <button
+            variant="primary"
+            className="view"
+            onClick={(e) => routeToMainReport()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-return-left"
+              viewBox="0 0 22 18"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+              />
+            </svg>
+            Main Report
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
