b9bbb7f69f0a15c4f7c0e57484dcac930c61136c
Abhi4201790 <116883713+Abhi4201790@users.noreply.github.com>
2023-06-28 09:17:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
@@ -7,24 +7,75 @@ import Staff from "./pages/staffs/Staff";
 import Chart from "./chart";
 import PatientTestPage from "./components/Test/PatientTestPage";
 import ReportPage from "./components/Report/ReportPage";
-import O2Report from "./pages/report/O2Report";
-import SleepReport from "./pages/report/SleepReport";
+import MainReport from "./pages/report/O2Report";
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const App = () => {
+  const [authenticatedUser, setAuthenticatedUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthenticatedUser(user);
+      } else {
+        setAuthenticatedUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const PrivateRoute = ({ path, element }) => {
+    if (loading) {
+      // Render a loading state while checking the authentication state
+      return <div>Loading...</div>;
+    } else if (authenticatedUser) {
+      // Render the component if the user is authenticated
+      return element;
+    } else {
+      // Redirect to the login page if the user is not authenticated
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/patient" element={<Patient />} />
-      <Route path="/staff" element={<Staff />} />
-      <Route path="/chart" element={<Chart />} />
-      <Route path="/patients/:patientId/tests" element={<PatientTestPage />} />
-      <Route path="/report/:patientId/:testId" element={<ReportPage />} />
-      <Route path="/mainreport/:patientId/:testId/" element={<O2Report />} />
-      <Route path ="/sleepReport" element={<SleepReport />} />
+      <Route
+        path="/patient"
+        element={<PrivateRoute element={<Patient />} />}
+      />
+      <Route
+        path="/staff"
+        element={<PrivateRoute element={<Staff />} />}
+      />
+      <Route
+        path="/chart"
+        element={<PrivateRoute element={<Chart />} />}
+      />
+      <Route
+        path="/patients/:patientId/tests"
+        element={<PrivateRoute element={<PatientTestPage />} />}
+      />
+      <Route
+        path="/report/:patientId/:testId"
+        element={<PrivateRoute element={<ReportPage />} />}
+      />
+      <Route
+        path="/mainreport/:patientId/:testId/"
+        element={<PrivateRoute element={<MainReport />} />}
+      />
     </Routes>
   );
-}
+};
 
 export default App;
